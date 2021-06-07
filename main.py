e89ba48674e38cbadbed046293f44751337932f8
Denish Goklani <golug24@gmail.com>
2021-06-07 19:55:12 +0530

    updated

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+def retrive_messages(channel_id):
+    headers = {
+        'authorization': '{Authorization(Check Readme)}'
+    }
+    r = requests.get(f'https://discord.com/api/v9/channels/{channel_id}/messages', headers=headers)
+    jsonn = json.loads(r.text)
+    for value in jsonn:
+        with open("{Path/to/PreExisting/fileName.txt}","a") as myfile:
+            myfile.write(value['content'])
+            myfile.write("\n")
+    
+
+retrive_messages('829019548190507048')
