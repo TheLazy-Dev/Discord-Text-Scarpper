368d0eb2f66dd56e958fac1672c272df1d57dd5f
Aditya Narendra <49760167+Ozzey@users.noreply.github.com>
2021-06-08 20:05:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from flask import Flask, render_template, request , redirect , url_for
 app = Flask(__name__)
 
 #_____________________________________________
-def retrive_messages(code , channel_id):
+#FORMATTED
+def formatted_messages(code , channel_id):
     messages =  []
     headers  = {'authorization': code}
     r        = requests.get(f'https://discord.com/api/v9/channels/{channel_id}/messages', headers=headers)
@@ -20,27 +21,52 @@ def retrive_messages(code , channel_id):
 
     messages.reverse()
     for message in messages:
-            with open(f'Scraped Messages/message_{channel_id}.txt' ,"a", encoding='utf-8') as myfile:
+            with open(f'Scraped Messages/Message_{channel_id}_Formatted.txt' ,"a", encoding='utf-8') as myfile:
                 myfile.write(message)
                 myfile.write("\n")
                 myfile.write("\n")
 
 #__________________________________________________
+#DEFAULT
+def retrive_messages(code , channel_id):
+    headers  = {'authorization': code}
+    r        = requests.get(f'https://discord.com/api/v9/channels/{channel_id}/messages', headers=headers)
+    jsonn    = json.loads(r.text)
 
+    for value in jsonn:
+        with open(f'Scraped Messages/Message_{channel_id}.txt' ,"a", encoding='utf-8') as myfile:
+            myfile.write(value['content'])
+            myfile.write("\n")
+
+#___________________________________________________
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == 'POST':
-        try:
-            auth_code  = request.form["code"]
-            cid = request.form["cid"]
-            retrive_messages(auth_code, cid)
-            print(auth_code)
-            print(cid)
-            return render_template('success.html')
+        print(request.form['parse_type'])
+        if request.form['parse_type'] == "formatted":
+            try:
+                auth_code  = request.form["code"]
+                cid = request.form["cid"]
+                formatted_messages(auth_code, cid)
+                print(auth_code)
+                print(cid)
+                return render_template('success.html')
 
-        except:
-            return render_template('error.html')
+            except:
+                return render_template('error.html')
+
+        else:
+            try:
+                auth_code  = request.form["code"]
+                cid = request.form["cid"]
+                retrive_messages(auth_code, cid)
+                print(auth_code)
+                print(cid)
+                return render_template('success.html')
+
+            except:
+                return render_template('error.html')
 
     else:
         return render_template('index.html')
